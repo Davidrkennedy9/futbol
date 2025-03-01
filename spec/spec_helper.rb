c7e0adeffa3599e518fe5c5778e25664c943d7f7
David Kennedy <davidkennedy@Davids-MacBook-Air.local>
2025-03-01 10:18:21 -0700

    Will make both stat_tracker and spec_file

---
+++ spec/spec_helper.rb
@@ -0,0 +1,5 @@
+require "./lib/stat_tracker"
+require "pry"
+require "simplecov"
+SimpleCov.start
+
---
+++ spec/stat_tracker_spec.rb
@@ -0,0 +1,24 @@
+require "spec_helper"
+
+RSpec.describe StatTracker do
+    before :each do
+     @game_path = './data/games.csv'
+     @team_path = './data/teams.csv'
+     @game_teams_path = './data/game_teams.csv'
+    
+     @locations = {
+       games: @game_path,
+       teams: @team_path,
+       game_teams: @game_teams_path
+     }
+
+     @stat_tracker = StatTracker.from_csv(@locations)
+    end
+    describe "initialize" do
+        it "exist" do
+         @stat_tracker = StatTracker.from_csv(@locations)
+
+         expect(@stat_tracker).to be(StatTracker)
+        end
+    end
+end  
