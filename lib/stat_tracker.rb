ed9f8fbfce04938daa353f2aba7904e429126d63
David Kennedy <davidkennedy@Davids-MacBook-Air.local>
2025-03-03 19:47:18 -0700

    Make the method for highest_total_score

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -57,4 +57,12 @@ class StatTracker
         
         new(games, teams, game_teams)
     end
+    def highest_total_score
+        score = []
+        @games.each do |game|
+           score_tracker = game[:away_goals] + game[:home_goals]
+           score << score_tracker
+        end
+        score.max 
+    end
 end
