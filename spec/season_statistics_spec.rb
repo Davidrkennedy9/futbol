264ccffbbf86eaafc1a4e124ba74a59bfd136777
Ecaterina Profir <ecaterina.profir@gmail.com>
2025-03-06 17:04:00 -0600

    added tests for the winningest coach method

---
+++ spec/season_statistics_spec.rb
@@ -0,0 +1,25 @@
+require 'rspec'
+require_relative '../lib/stat_tracker'
+require_relative '../lib/season_statistics'
+
+
+RSpec.describe SeasonStatistics do
+  before(:each) do
+    @game_path = './data/games.csv'
+     @team_path = './data/teams.csv'
+     @game_teams_path = './data/game_teams.csv'
+    
+     @locations = {
+       games: @game_path,
+       teams: @team_path,
+       game_teams: @game_teams_path
+     }
+
+
+    @stat_tracker = StatTracker.from_csv(@locations)
+  end
+  it "#winningest_coach" do
+    expect(@stat_tracker.winningest_coach("20132014")).to eq "Claude Julien"
+    expect(@stat_tracker.winningest_coach("20142015")).to eq "Alain Vigneault"
+  end
+end
