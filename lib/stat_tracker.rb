858cd5e4c4cdf018199ad5f25a1ce1d82aaa9318
Ecaterina Profir <ecaterina.profir@gmail.com>
2025-03-07 11:23:45 -0600

    included the seasonstatistics module

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -1,10 +1,16 @@
 require 'csv'
+require_relative 'game'
+require_relative 'team'
+require_relative 'game_team'
+require_relative 'season_statistics'
 class StatTracker
     attr_reader :games, :teams, :game_teams
 
-    def initialize(games, teams, game_teams)
+    include SeasonStatistics
 
-       @games = games
+    def initialize(games, teams, game_teams) #initialize runs automatically whenever we create a new StatTracker object.
+
+       @games = games #Storing those objects in instance variables (@games, @teams, @game_teams).
        @teams = teams
        @game_teams = game_teams
 
@@ -12,7 +18,7 @@ class StatTracker
 
     def self.from_csv(locations)
 
-        games = CSV.read(locations[:games], headers: true, header_converters: :symbol).map do |row|
+        games = CSV.read(locations[:games], headers: true, header_converters: :symbol).map do |row| #Reading CSV files and converting each row into an object (Game, Team, or GameTeam).
             Game.new(row)   
         end
 
@@ -25,7 +31,8 @@ class StatTracker
             GameTeam.new(row)
         end
         
-        new(games, teams, game_teams)
+        new(games, teams, game_teams) #creates a new StatTracker object. stores the parsed data inside it. StatTracker now contains all games, teams, and game-teams as objects.
+        
 
     end 
 end
