2b9d6d52f09685ee9e9c6ddb1b49e1160899d4ef
Ecaterina Profir <ecaterina.profir@gmail.com>
2025-03-06 18:09:38 -0600

    created a test for the most accurate team method

--- lib/season_statistics.rb
+++ lib/season_statistics.rb
@@ -32,5 +32,34 @@ module SeasonStatistics
     # retun coach's name, or nil if no coach found
     best_coach&.first
   end
+
+  def worst_coach(season_id)
+    #need to get all games for the  season in test
+    season_games = @games.select { |game| game.season == season_id }
+  
+     #get game ids for that seasonv
+    season_game_ids = season_games.map { |game| game.game_id }
+  
+    #all teams that played in those games
+    season_game_teams = @game_teams.select { |game_team| season_game_ids.include?(game_team.game_id) }
+  
+    #  a hash to track wins and total games for each coach
+    coach_stats = Hash.new { |hash, key| hash[key] = { wins: 0, total_games: 0 } }
+  
+    # count total games and wins for each coach
+    season_game_teams.each do |game_team|
+      coach = game_team.head_coach
+      coach_stats[coach][:total_games] += 1
+      coach_stats[coach][:wins] += 1 if game_team.result == "WIN"
+    end
+  
+    # find the coach with the lowest win percentage
+    worst_coach = coach_stats.min_by do |coach, stats|
+      stats[:wins].to_f / stats[:total_games]
+    end
+  
+    # return only the coach's name, or nil if no coach is found
+    worst_coach&.first
+  end
 end
 
